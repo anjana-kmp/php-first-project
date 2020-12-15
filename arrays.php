d95bee82b05cc173ae77e41abbf95644039e0eb4
anjana-kmp <71718802+anjana-kmp@users.noreply.github.com>
2020-12-15 21:24:39 +0530

    adding arrays with push and pop functions.

--- arrays.php
+++ arrays.php
@@ -1,5 +1,30 @@
 <?php
     $myarr = array('Physics','Chemistry','Biology','English');
-    echo ($myvar)
+    print_r($myarr);
+   
+    echo "<ol>";
+    foreach($myarr as $itm){
+        echo "<li>$itm</li>";
+    }
+    echo "</ol>";
+    echo '<link rel="stylesheet" href="style.css">';
+    array_push($myarr,'History');
+    echo "<h3>After array push</h3>";
+    echo "<ul>";
+    foreach($myarr as $itm){
+        echo "<li>$itm</li>";
+    }
+    echo "</ul>";
+    echo "<h3>After array pop</h3>";
+    array_pop($myarr);
+    array_pop($myarr);
+    array_pop($myarr);
+    echo "<ul>";
+    foreach($myarr as $itm){
+        echo "<li>$itm</li>";
+    }
+    echo "</ul>";
+    
+
 
 ?>
